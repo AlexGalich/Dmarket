aeae97d89a0a7d78af2832d27e6890c64fc00381
Alex <random@gmail.com>
2023-06-04 03:48:37 +0200

    status code

--- dm_evaluation.py
+++ dm_evaluation.py
@@ -9,6 +9,7 @@ def request_sales_history(item_encoded):
     header = {'authorization': 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlNjE5N2Y4Yi1iMTVhLTQ0MmMtYWZmYi0yYmM0ZThkMWJkZTkiLCJleHAiOjE2ODU2MzgxMDMsImlhdCI6MTY4MzA0NjEwMywic2lkIjoiNzNlMGVlMzQtNmI3ZC00ZWM4LTgyMjktN2UzMjI2MzM1NDZjIiwidHlwIjoiYWNjZXNzIiwiaWQiOiIxNjViMWE3Yy1kZDZmLTQ0YzYtYjI2MC02OGU1NTA4OTFiMTAiLCJwdmQiOiJtcCIsInBydCI6IjIzMDUiLCJhdHRyaWJ1dGVzIjp7InNhZ2Ffd2FsbGV0X2FkZHJlc3MiOiIweDEwMGYzZTBkMGRENGFCMTc0MmZFZUYyMDc3NzA0RTcyMzRjNWZiZGMiLCJhY2NvdW50X2lkIjoiMzZjOWM5MTMtZDBhZC00ZGNjLTk0NTMtMTI0YjJmMTQ4ZTk5Iiwid2FsbGV0X2lkIjoiNzZhZjAyNTkyYzNkZGZkMWQ0YWU5NmZiZDU2YzhkYmY5ZWY4ZmU1NTAyNmVkYmUyODgzMGIyYTdlNzE5NGRlMyJ9fQ.ZC5edMIOC4YR7dWhjvNime96rKCm2kPxswskI1wiceENzRKmJBFwKsAXUUz26zv1YOw7A8ACtia3OLTX6emTaQ'}
     market_response = requests.get(f'https://api.dmarket.com/marketplace-api/v1/sales-history?Title={item_encoded}&GameID=a8db&Period=7D&Currency=USD',headers=header)
     try:
+        print(market_response.status_code)
         hist = json.loads(market_response.text)
       
         return hist['SalesHistory']
